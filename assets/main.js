3b853292fcf0bb4ba4dde26def93f7f6c9849820
gilfoyle <feedback@ikeepmyresolutions.com>
2018-12-09 21:07:48 +0500

    add email form

--- assets/main.js
+++ assets/main.js
@@ -5,3 +5,10 @@ $(document).on('click', 'a[href^="#"]', function (event) {
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
+
+$(document).ready(function() {
+    $("#reminder-form").submit(function(event) {
+        event.preventDefault();
+        console.log( "Handler for .submit() called.");
+    });
+})
